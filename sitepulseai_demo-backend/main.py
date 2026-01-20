c53db6834487a62d195f32859f1b5ec0a1a1707e
Jerome Reaves <jhajhawebdev@gmail.com>
2026-01-20 14:57:30 -0500

    Fix SSL router prefix and correct /ssl/state endpoint registration

--- sitepulseai_demo-backend/main.py
+++ sitepulseai_demo-backend/main.py
@@ -44,7 +44,7 @@ app.add_middleware(
         "https://www.sitepulseai.com",
         "http://127.0.0.1:5500"
     ],
-    allow_origins=["*"],
+    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
