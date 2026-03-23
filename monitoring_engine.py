221f6f376c8972a59197ededbad67d4c19357ce9
Jerome Reaves <jhajhawebdev@gmail.com>
2026-03-23 14:10:33 -0400

    monitoring_engine.py

---
+++ monitoring_engine.py
@@ -0,0 +1,14 @@
+import time
+from monitor import run_full_check
+from site_manager import get_all_sites
+
+def start_monitoring():
+    print("Monitoring Engine: ACTIVE")
+
+    while True:
+        sites = get_all_sites()
+
+        for site in sites:
+            run_full_check(site)
+
+        time.sleep(300)  # 5 minutes
