b7b51f1e438dbc7245b510bd8cf313bd2996992c
Jerome Reaves <jhajhawebdev@gmail.com>
2026-03-28 12:31:14 -0400

    Enable CORS

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ app = FastAPI(
 # -----------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # 🔥 allow all for now
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
