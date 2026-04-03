16ab3d3b1d4c6db1e70c5c2f2919470e95f425dd
Jerome Reaves <jhajhawebdev@gmail.com>
2026-04-03 18:16:40 -0400

    main.py

--- main.py
+++ main.py
@@ -79,13 +79,11 @@ app = FastAPI()
 # --- CORS ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-    ],
+    allow_origins=["*"],  # later restrict to your domain
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+
 )
 
 
