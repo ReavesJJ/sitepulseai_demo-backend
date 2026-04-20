f55ff160aa1f763d03099c5f66e6f944af1aacca
Jerome Reaves <jhajhawebdev@gmail.com>
2026-04-20 19:11:46 -0400

    main.py

--- main.py
+++ main.py
@@ -12,12 +12,7 @@ from fastapi import FastAPI, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import requests
-from fastapi import FastAPI, Depends
-from license_enforcer import enforce_license
 
-app = FastAPI(
-    dependencies=[Depends(enforce_license)]
-)
 
 
 
